8798361483dcb064c6341b8105efcbacdd205d98
sleem22022s-source <sleem22022s@gmail.com>
2026-08-01 22:36:26 +0300

    مش عارف انا عملت ايه
    مش عارف انا عملت ايه

--- القائمات/القائمه الرائيسيه/القائمه_الرائيسيه.gd
+++ القائمات/القائمه الرائيسيه/القائمه_الرائيسيه.gd
@@ -1,19 +1,19 @@
 extends Control
 
+# قم بسحب نود قائمة الإعدادات هنا من شجرة النودات (Node Tree) في المحرر
 @export var options_menu: Control
-
-
-
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
-
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_opsions_pressed() -> void:
-	hide() # إخفاء قائمة التوقف
+	# 1. إذا كنت رابط نود الإعدادات داخل نفس المشهد عن طريق Inspector
 	if options_menu:
+		options_menu.show() # إظهار قائمة الإعدادات فوق القائمة الحالية
+		hide() # إخفاء القائمة الرئيسية
+	else:
+		# 2. الانتقال لمشهد آخر في حالة عدم الربط
 		get_tree().change_scene_to_file("res://القائمات/القائمه الاعدادات/options_menu.tscn")
- 
